5fef015852667075af3f43946aaf1e788fd05bb0
Jay Gabriels <jgabriels30@gmail.com>
2018-03-29 08:10:00 -0700

    getting clone urls

--- cmd/clone.go
+++ cmd/clone.go
@@ -19,22 +19,53 @@ func authSetup() (*github.Client, *github.RepositoryListByOrgOptions) {
 
 	opt := &github.RepositoryListByOrgOptions{
 		Type:        "all",
-		ListOptions: github.ListOptions{PerPage: 1000},
+		ListOptions: github.ListOptions{Page: 2, PerPage: 1000},
 	}
 
 	return client, opt
 }
 
-// CloneAllReposByOrg clones all repos for a given org
-func CloneAllReposByOrg() {
-	client, opt := authSetup()
-	repos, _, err := client.Repositories.ListByOrg(context.Background(), os.Args[1], opt)
-	if err != nil {
-		fmt.Print("Oh no error city: ", err)
+// TODO: Figure out how to use go channels for this
+func getAllOrgCloneUrls() ([]string, error) {
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+
+	opt := &github.RepositoryListByOrgOptions{
+		Type:        "all",
+		ListOptions: github.ListOptions{PerPage: 100},
 	}
 
-	fmt.Println(repos)
+	// get all pages of results
+	var allRepos []*github.Repository
+	for {
+		repos, resp, err := client.Repositories.ListByOrg(context.Background(), os.Args[1], opt)
+		if err != nil {
+			return nil, err
+		}
+		allRepos = append(allRepos, repos...)
+		if resp.NextPage == 0 {
+			break
+		}
+		opt.Page = resp.NextPage
+	}
+	cloneUrls := []string{}
+	for _, repo := range allRepos {
+		fmt.Println(*repo.CloneURL)
+	}
+
+	return cloneUrls, nil
 }
+
+// CloneAllReposByOrg clones all repos for a given org
+func CloneAllReposByOrg() {
+	getAllOrgCloneUrls()
+}
+
+// TODO: Clone via http or ssh flag
 
 // Could clone all repos on a user
 // orgs, _, err := client.Organizations.List(context.Background(), "willnorris", nil)
