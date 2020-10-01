9ef2637026e74b194eaca610f24ef1a4053e1fb1
6543 <6543@obermui.de>
2020-10-01 05:38:21 +0200

    Dedub gitlab.go

--- internal/gitlab/gitlab.go
+++ internal/gitlab/gitlab.go
@@ -36,48 +36,8 @@ func GetOrgRepos(targetOrg string) ([]repo.Data, error) {
 			return []repo.Data{}, err
 		}
 
-		// List all the projects we've found so far.
-		for _, p := range ps {
-
-			if os.Getenv("GHORG_SKIP_ARCHIVED") == "true" {
-				if p.Archived == true {
-					continue
-				}
-			}
-
-			if os.Getenv("GHORG_SKIP_FORKS") == "true" {
-				if p.ForkedFromProject != nil {
-					continue
-				}
-			}
-
-			if os.Getenv("GHORG_MATCH_PREFIX") != "" {
-				repoName := strings.ToLower(p.Name)
-				foundPrefix := false
-				pfs := strings.Split(os.Getenv("GHORG_MATCH_PREFIX"), ",")
-				for _, p := range pfs {
-					if strings.HasPrefix(repoName, strings.ToLower(p)) {
-						foundPrefix = true
-					}
-				}
-				if foundPrefix == false {
-					continue
-				}
-			}
-
-			r := repo.Data{}
-
-			r.Path = p.PathWithNamespace
-			if os.Getenv("GHORG_CLONE_PROTOCOL") == "https" {
-				r.CloneURL = addTokenToHTTPSCloneURL(p.HTTPURLToRepo, os.Getenv("GHORG_GITLAB_TOKEN"))
-				r.URL = p.HTTPURLToRepo
-				repoData = append(repoData, r)
-			} else {
-				r.CloneURL = p.SSHURLToRepo
-				r.URL = p.SSHURLToRepo
-				repoData = append(repoData, r)
-			}
-		}
+		// filter from all the projects we've found so far.
+		repoData = append(repoData, filter(ps)...)
 
 		// Exit the loop when we've seen all pages.
 		if resp.CurrentPage >= resp.TotalPages {
@@ -128,40 +88,8 @@ func GetUserRepos(targetUsername string) ([]repo.Data, error) {
 			return []repo.Data{}, err
 		}
 
-		// List all the projects we've found so far.
-		for _, p := range ps {
-			if os.Getenv("GHORG_SKIP_ARCHIVED") == "true" {
-				if p.Archived == true {
-					continue
-				}
-			}
-
-			if os.Getenv("GHORG_MATCH_PREFIX") != "" {
-				repoName := strings.ToLower(p.Name)
-				foundPrefix := false
-				pfs := strings.Split(os.Getenv("GHORG_MATCH_PREFIX"), ",")
-				for _, p := range pfs {
-					if strings.HasPrefix(repoName, strings.ToLower(p)) {
-						foundPrefix = true
-					}
-				}
-				if foundPrefix == false {
-					continue
-				}
-			}
-
-			r := repo.Data{}
-			r.Path = p.PathWithNamespace
-			if os.Getenv("GHORG_CLONE_PROTOCOL") == "https" {
-				r.CloneURL = addTokenToHTTPSCloneURL(p.HTTPURLToRepo, os.Getenv("GHORG_GITLAB_TOKEN"))
-				r.URL = p.HTTPURLToRepo
-				cloneData = append(cloneData, r)
-			} else {
-				r.CloneURL = p.SSHURLToRepo
-				r.URL = p.SSHURLToRepo
-				cloneData = append(cloneData, r)
-			}
-		}
+		// filter from all the projects we've found so far.
+		cloneData = append(cloneData, filter(ps)...)
 
 		// Exit the loop when we've seen all pages.
 		if resp.CurrentPage >= resp.TotalPages {
@@ -178,4 +106,50 @@ func GetUserRepos(targetUsername string) ([]repo.Data, error) {
 func addTokenToHTTPSCloneURL(url string, token string) string {
 	splitURL := strings.Split(url, "https://")
 	return "https://oauth2:" + token + "@" + splitURL[1]
+}
+
+func filter(ps []*gitlab.Project) []repo.Data {
+	var repoData []repo.Data
+	for _, p := range ps {
+
+		if os.Getenv("GHORG_SKIP_ARCHIVED") == "true" {
+			if p.Archived == true {
+				continue
+			}
+		}
+
+		if os.Getenv("GHORG_SKIP_FORKS") == "true" {
+			if p.ForkedFromProject != nil {
+				continue
+			}
+		}
+
+		if os.Getenv("GHORG_MATCH_PREFIX") != "" {
+			repoName := strings.ToLower(p.Name)
+			foundPrefix := false
+			pfs := strings.Split(os.Getenv("GHORG_MATCH_PREFIX"), ",")
+			for _, p := range pfs {
+				if strings.HasPrefix(repoName, strings.ToLower(p)) {
+					foundPrefix = true
+				}
+			}
+			if foundPrefix == false {
+				continue
+			}
+		}
+
+		r := repo.Data{}
+
+		r.Path = p.PathWithNamespace
+		if os.Getenv("GHORG_CLONE_PROTOCOL") == "https" {
+			r.CloneURL = addTokenToHTTPSCloneURL(p.HTTPURLToRepo, os.Getenv("GHORG_GITLAB_TOKEN"))
+			r.URL = p.HTTPURLToRepo
+			repoData = append(repoData, r)
+		} else {
+			r.CloneURL = p.SSHURLToRepo
+			r.URL = p.SSHURLToRepo
+			repoData = append(repoData, r)
+		}
+	}
+	return repoData
 }
