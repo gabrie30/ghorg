ab69f89b28dc3a883a4dfbf1812f7460838fdbec
6543 <6543@obermui.de>
2020-10-21 00:55:07 +0200

    Vendor: xanzy/go-gitlab v0.33.0 -> v0.38.2 (#104)

--- vendor/github.com/xanzy/go-gitlab/event_webhook_types.go
+++ vendor/github.com/xanzy/go-gitlab/event_webhook_types.go
@@ -85,6 +85,7 @@ type TagEvent struct {
 	UserID      int    `json:"user_id"`
 	UserName    string `json:"user_name"`
 	UserAvatar  string `json:"user_avatar"`
+	UserEmail   string `json:"user_email"`
 	ProjectID   int    `json:"project_id"`
 	Message     string `json:"message"`
 	Project     struct {
@@ -593,6 +594,10 @@ type MergeEvent struct {
 			Previous int `json:"previous"`
 			Current  int `json:"current"`
 		} `json:"source_project_id"`
+		StateID struct {
+			Previous int `json:"previous"`
+			Current  int `json:"current"`
+		} `json:"state_id"`
 		TargetBranch struct {
 			Previous string `json:"previous"`
 			Current  string `json:"current"`
