7b199540e081de3fd3cb550058358c11408c8bdf
Jay Gabriels <jgabriels30@gmail.com>
2021-04-02 09:30:07 -0700

    Add clone me option for github

--- cmd/version.go
+++ cmd/version.go
@@ -15,6 +15,6 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of Ghorg",
 	Long:  `All software has versions. This is Ghorg's`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("1.5.1")
+		fmt.Println("1.5.2")
 	},
 }
