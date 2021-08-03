e8cd1a5f95403f177ec30aca03d6d1c9b5a3bdc6
Jay Gabriels <jgabriels30@gmail.com>
2021-08-02 19:02:48 -0700

    Update github_integration_tests.sh

--- scripts/github_integration_tests.sh
+++ scripts/github_integration_tests.sh
@@ -6,14 +6,26 @@ echo "Running GitHub Integration Tests"
 
 cp ./ghorg /usr/local/bin
 
+GITHUB_ORG=underdeveloped
+
 ghorg version
 
-ghorg clone underdeveloped --token=$GITHUB_TOKEN --path=/tmp --output-dir=normal_clone
+ghorg clone $GITHUB_ORG --token=$GITHUB_TOKEN
 
-if [ -e /tmp/normal_clone ]
+if [ -e $HOME/ghorg/$GITHUB_ORG ]
 then
-    echo "Pass: Normal Clone"
+    echo "Pass: github org clone"
 else
-    echo "Fail: Normal Clone"
+    echo "Fail: github org clone"
+    exit 1
+fi
+
+ghorg clone $GITHUB_ORG --token=$GITHUB_TOKEN --path=/tmp --output-dir=testing_output_dir
+
+if [ -e /tmp/testing_output_dir ]
+then
+    echo "Pass: github org clone custom output directories"
+else
+    echo "Fail: github org clone custom output directories"
     exit 1
 fi
