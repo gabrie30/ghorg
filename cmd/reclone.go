334e7306166edc4937dfe7f73e944f2c29ae88d3
Jay Gabriels <gabrie30@users.noreply.github.com>
2022-07-31 12:56:01 -0700

    Add/reclone (#221)
    * Add ghorg reclone command

---
+++ cmd/reclone.go
@@ -0,0 +1,88 @@
+package cmd
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os/exec"
+	"strings"
+
+	"github.com/gabrie30/ghorg/colorlog"
+	"github.com/gabrie30/ghorg/configs"
+	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
+)
+
+var reCloneCmd = &cobra.Command{
+	Use:   "reclone",
+	Short: "Reruns one, multiple, or all preconfigured clones from configuration set in $HOME/.config/ghorg/reclone.yaml",
+	Long:  `Allows you to set preconfigured clone commands for handling multiple users/orgs at once. See README.md at https://github.com/gabrie30/ghorg for setup information.`,
+	Run:   reCloneFunc,
+}
+
+type ReClone struct {
+	Cmd string `yaml:"cmd"`
+}
+
+func reCloneFunc(cmd *cobra.Command, argz []string) {
+	path := configs.GhorgReCloneLocation()
+	yamlBytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		e := fmt.Sprintf("ERROR: parsing reclone.yaml, error: %v", err)
+		colorlog.PrintErrorAndExit(e)
+	}
+
+	mapOfReClones := make(map[string]ReClone)
+
+	err = yaml.Unmarshal(yamlBytes, &mapOfReClones)
+	if err != nil {
+		e := fmt.Sprintf("ERROR: unmarshaling reclone.yaml, error:%v", err)
+		colorlog.PrintErrorAndExit(e)
+	}
+
+	if len(argz) == 0 {
+		for _, key := range mapOfReClones {
+			runReClone(key)
+		}
+	} else {
+		for _, arg := range argz {
+			if _, ok := mapOfReClones[arg]; !ok {
+				e := fmt.Sprintf("ERROR: The key %v was not found in reclone.yaml", arg)
+				colorlog.PrintErrorAndExit(e)
+			}
+			runReClone(mapOfReClones[arg])
+		}
+	}
+
+}
+
+func runReClone(rc ReClone) {
+	// make sure command starts with ghorg clone
+	splitCommand := strings.Split(rc.Cmd, " ")
+	ghorg, clone, remainingCommand := splitCommand[0], splitCommand[1], splitCommand[1:]
+
+	if ghorg != "ghorg" || clone != "clone" {
+		colorlog.PrintErrorAndExit("ERROR: Only ghorg clone commands are permitted in your reclone.yaml")
+	}
+	ghorgClone := exec.Command("ghorg", remainingCommand...)
+
+	stdout, err := ghorgClone.StdoutPipe()
+	if err != nil {
+		e := fmt.Sprintf("ERROR: Problem with piping to stdout, err: %v", err)
+		colorlog.PrintErrorAndExit(e)
+	}
+
+	err = ghorgClone.Start()
+
+	if err != nil {
+		e := fmt.Sprintf("ERROR: Running ghorg clone cmd: %v, err: %v", rc.Cmd, err)
+		colorlog.PrintErrorAndExit(e)
+	}
+
+	scanner := bufio.NewScanner(stdout)
+	for scanner.Scan() {
+		m := scanner.Text()
+		fmt.Println(m)
+	}
+	ghorgClone.Wait()
+}
