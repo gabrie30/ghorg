04c3969fc631bc1d648fec5ef1a2f8921b62314b
Jay Gabriels <jgabriels30@gmail.com>
2022-08-06 20:38:49 -0700

    Fix gitlab clone repo name collision issue

--- scripts/local-gitlab/clone.sh
+++ scripts/local-gitlab/clone.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 BASE_URL="http://gitlab.example.com"
-TOKEN=${1:-'EVvbmz5qb28ok-rU-zo5'}
+TOKEN=${1:-'yYPQd9zVy3hvMqsuK13-'}
 
-ghorg clone all-groups --scm=gitlab --base-url=${BASE_URL} --token=$TOKEN --preserve-dir --concurrency=25 --output-dir=local-gitlab-v15-repos
+ghorg clone all-groups --scm=gitlab --base-url=${BASE_URL} --token=$TOKEN --preserve-dir --output-dir=local-gitlab-v15-repos
 
-ghorg clone all-groups --scm=gitlab --base-url=${BASE_URL} --token=$TOKEN --concurrency=25 --output-dir=local-gitlab-v15-repos-flat
+ghorg clone all-groups --scm=gitlab --base-url=${BASE_URL} --token=$TOKEN --output-dir=local-gitlab-v15-repos-flat
+
+ghorg clone root --clone-type=user --scm=gitlab --base-url=${BASE_URL} --token=$TOKEN --output-dir=local-gitlab-v15-root-user-repos --prune --prune-no-confirm
+
+ghorg clone all-groups --scm=gitlab --base-url=${BASE_URL} --token=$TOKEN --backup --clone-wiki --output-dir=local-gitlab-v15-backup
+
+ghorg clone group1 --scm=gitlab --base-url=${BASE_URL} --token=$TOKEN --backup --output-dir=local-gitlab-v15-group1-backup
+
+ghorg clone group1 --scm=gitlab --base-url=${BASE_URL} --token=$TOKEN --output-dir=local-gitlab-v15-group1
