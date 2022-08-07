cb5805e88a8adeb567a671b23646d5c8666e68d3
Jay Gabriels <gabrie30@users.noreply.github.com>
2022-08-07 15:57:44 -0700

    Update gitlab local setup (#227)

--- scripts/local-gitlab/run-ee.sh
+++ scripts/local-gitlab/run-ee.sh
@@ -9,29 +9,23 @@
 
 # make sure 127.0.0.1 gitlab.example.com is added to your /etc/hosts
 
-export GITLAB_IMAGE_TAG=latest
-export GITLAB_HOME=$HOME/Desktop/ghorg/local-gitlab-ee-data-$GITLAB_IMAGE_TAG
+GITLAB_IMAGE_TAG=$1
+GITLAB_HOME=$2
+GITLAB_HOST=$3
+
+echo ""
+echo "Starting fresh install of GitLab Enterprise Edition, using tag: ${GITLAB_IMAGE_TAG}"
+echo "Removing any previous install at path: ${GITLAB_HOME}"
+echo ""
+
+rm -rf "${GITLAB_HOME}"
 
 docker run \
-  --hostname gitlab.example.com \
+  -d=true \
+  --hostname "${GITLAB_HOST}" \
   --publish 443:443 --publish 80:80 --publish 22:22 \
   --name gitlab \
-  --restart always \
-  --volume $GITLAB_HOME/config:/etc/gitlab \
-  --volume $GITLAB_HOME/logs:/var/log/gitlab \
-  --volume $GITLAB_HOME/data:/var/opt/gitlab \
-  gitlab/gitlab-ee:$GITLAB_IMAGE_TAG
-
-# Once running get pw with
-docker exec -it gitlab grep 'Password:' /etc/gitlab/initial_root_password
-
-# Login at
-
-# http://gitlab.example.com/users/sign_in
-# username: root
-
-# Create an api token
-# http://gitlab.example.com/-/profile/personal_access_tokens
-
-# seed new instance using
-# ./scripts/local-gitlab/seed.sh $TOKEN
+  --volume "${GITLAB_HOME}"/config:/etc/gitlab \
+  --volume "${GITLAB_HOME}"/logs:/var/log/gitlab \
+  --volume "${GITLAB_HOME}"/data:/var/opt/gitlab \
+  gitlab/gitlab-ee:"${GITLAB_IMAGE_TAG}"
--- scripts/local-gitlab/seed.sh
+++ scripts/local-gitlab/seed.sh
@@ -2,56 +2,58 @@
 
 # https://docs.gitlab.com/ee/install/docker.html#install-gitlab-using-docker-engine
 
-TOKEN=${1:-'yYPQd9zVy3hvMqsuK13-'}
-BASE_URL="http://gitlab.example.com"
+TOKEN=$1
+GITLAB_URL=$2
 
 # Create 3 groups, namespace_id will start at 4
 curl --request POST --header "PRIVATE-TOKEN: $TOKEN" \
     --header "Content-Type: application/json" \
     --data '{"path": "group1", "name": "group1" }' \
-    "${BASE_URL}/api/v4/groups"
+    "${GITLAB_URL}/api/v4/groups"
 
 curl --request POST --header "PRIVATE-TOKEN: $TOKEN" \
     --header "Content-Type: application/json" \
     --data '{"path": "group2", "name": "group2" }' \
-    "${BASE_URL}/api/v4/groups"
+    "${GITLAB_URL}/api/v4/groups"
 
 curl --request POST --header "PRIVATE-TOKEN: $TOKEN" \
     --header "Content-Type: application/json" \
     --data '{"path": "group3", "name": "group3" }' \
-    "${BASE_URL}/api/v4/groups"
+    "${GITLAB_URL}/api/v4/groups"
 
 curl --request POST --header "PRIVATE-TOKEN: $TOKEN" \
     --header "Content-Type: application/json" \
     --data '{"path": "group4", "name": "group4" }' \
-    "${BASE_URL}/api/v4/groups"
+    "${GITLAB_URL}/api/v4/groups"
 
 # create repos for user
 for ((a=0; a <= 10 ; a++))
 do
-    curl --header "PRIVATE-TOKEN: $TOKEN" -X POST "${BASE_URL}/api/v4/projects?name=baz${a}&initialize_with_readme=true"
+    curl --header "PRIVATE-TOKEN: $TOKEN" -X POST "${GITLAB_URL}/api/v4/projects?name=baz${a}&initialize_with_readme=true"
 done
 
 # create repos in group1
 for ((a=0; a <= 10 ; a++))
 do
-    curl --header "PRIVATE-TOKEN: $TOKEN" -X POST "${BASE_URL}/api/v4/projects?name=baz${a}&namespace_id=4&initialize_with_readme=true"
+    curl --header "PRIVATE-TOKEN: $TOKEN" -X POST "${GITLAB_URL}/api/v4/projects?name=baz${a}&namespace_id=4&initialize_with_readme=true"
 done
 
 # create repos in group2
 for ((a=0; a <= 10 ; a++))
 do
-    curl --header "PRIVATE-TOKEN: $TOKEN" -X POST "${BASE_URL}/api/v4/projects?name=baz${a}&namespace_id=5&initialize_with_readme=true"
+    curl --header "PRIVATE-TOKEN: $TOKEN" -X POST "${GITLAB_URL}/api/v4/projects?name=baz${a}&namespace_id=5&initialize_with_readme=true"
 done
 
 # create repos in group3
 for ((a=0; a <= 10 ; a++))
 do
-    curl --header "PRIVATE-TOKEN: $TOKEN" -X POST "${BASE_URL}/api/v4/projects?name=baz${a}&namespace_id=6&initialize_with_readme=true"
+    curl --header "PRIVATE-TOKEN: $TOKEN" -X POST "${GITLAB_URL}/api/v4/projects?name=baz${a}&namespace_id=6&initialize_with_readme=true"
 done
 
 # create repos in group3
 for ((a=0; a <= 10 ; a++))
 do
-    curl --header "PRIVATE-TOKEN: $TOKEN" -X POST "${BASE_URL}/api/v4/projects?name=baz${a}&namespace_id=7&initialize_with_readme=true&wiki_enabled=true"
+    curl --header "PRIVATE-TOKEN: $TOKEN" -X POST "${GITLAB_URL}/api/v4/projects?name=baz${a}&namespace_id=7&initialize_with_readme=true&wiki_enabled=true"
 done
+
+./scripts/local-gitlab/clone.sh "${TOKEN}" "${GITLAB_URL}"
