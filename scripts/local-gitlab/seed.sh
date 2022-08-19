942d86f8774c09fde30fb23de64f12df53a4aa81
Jay Gabriels <jgabriels30@gmail.com>
2022-08-19 13:24:34 -0700

    Update seed.sh

--- scripts/local-gitlab/seed.sh
+++ scripts/local-gitlab/seed.sh
@@ -5,7 +5,7 @@
 TOKEN=$1
 GITLAB_URL=$2
 
-# Create 3 groups, namespace_id will start at 4
+# Create 2 groups, namespace_id will start at 4
 curl --request POST --header "PRIVATE-TOKEN: $TOKEN" \
     --header "Content-Type: application/json" \
     --data '{"path": "group1", "name": "group1" }' \
@@ -16,20 +16,26 @@ curl --request POST --header "PRIVATE-TOKEN: $TOKEN" \
     --data '{"path": "group2", "name": "group2" }' \
     "${GITLAB_URL}/api/v4/groups"
 
+sleep 1
+
 # create repos for user
-for ((a=0; a <= 10 ; a++))
+for ((a=0; a <= 3 ; a++))
 do
     curl --header "PRIVATE-TOKEN: $TOKEN" -X POST "${GITLAB_URL}/api/v4/projects?name=baz${a}&initialize_with_readme=true"
 done
 
+sleep 1
+
 # create repos in group1
-for ((a=0; a <= 10 ; a++))
+for ((a=0; a <= 3 ; a++))
 do
     curl --header "PRIVATE-TOKEN: $TOKEN" -X POST "${GITLAB_URL}/api/v4/projects?name=baz${a}&namespace_id=4&initialize_with_readme=true"
 done
 
+sleep 1
+
 # create repos in group2
-for ((a=0; a <= 10 ; a++))
+for ((a=0; a <= 3 ; a++))
 do
     curl --header "PRIVATE-TOKEN: $TOKEN" -X POST "${GITLAB_URL}/api/v4/projects?name=baz${a}&namespace_id=5&initialize_with_readme=true"
 done
