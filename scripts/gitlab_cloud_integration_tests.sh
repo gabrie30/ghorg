ee356915352a14b164cdeb64b9a1bd5b03f99086
gabrie30 <jgabriels30@gmail.com>
2025-05-14 21:50:43 -0700

    Update gitlab_cloud_integration_tests.sh

--- scripts/gitlab_cloud_integration_tests.sh
+++ scripts/gitlab_cloud_integration_tests.sh
@@ -144,9 +144,9 @@ else
 fi
 
 # PRUNE AND PRESERVE DIR
-ghorg clone $GITLAB_GROUP --token="${GITLAB_TOKEN}" --scm=gitlab --prune --preserve-dir
+ghorg clone $GITLAB_GROUP --token="${GITLAB_TOKEN}" --scm=gitlab --prune --preserve-dir --prune-no-confirm
 git init "${HOME}"/ghorg/"${GITLAB_GROUP}"/wayne-enterprises/wayne-industries/prunable
-ghorg clone $GITLAB_GROUP --token="${GITLAB_TOKEN}" --scm=gitlab --prune --preserve-dir
+ghorg clone $GITLAB_GROUP --token="${GITLAB_TOKEN}" --scm=gitlab --prune --preserve-dir --prune-no-confirm
 
 if [ -e "${HOME}"/ghorg/"${GITLAB_GROUP}"/wayne-enterprises/wayne-industries/microservice ] && \
     [ ! -e "${HOME}"/ghorg/"${GITLAB_GROUP}"/wayne-enterprises/wayne-industries/prunable ]
